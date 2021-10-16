8e53bd8de1f17230b84bd060b24abdec0d0a1259
Priyanshu Gaikwad <63440566+Pr1-xD@users.noreply.github.com>
2021-10-16 23:26:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Home'
 
 function App() {
   return (
     <div className="App">
       <h1>Home</h1>
     <h1>Home</h1>
-    <h1>Home</h1>
-    <h1>Home</h1>
-    <h1>Home</h1>
-    <h1>Home</h1>
-    <h1>Home</h1>
-    <h1>Home</h1>
+   <Home/>
     </div>
   );
 }
